ecdc979f2d75add287a017aca2360273f36876ac
ezhov-da <ezhdenis@yandex.ru>
2020-12-27 23:24:26 +0300

    feat: initial

--- src/test/kotlin/ru/ezhov/duplicate/view/PaginationServiceTest.kt
+++ src/test/kotlin/ru/ezhov/duplicate/view/PaginationServiceTest.kt
@@ -5,19 +5,19 @@ import org.junit.jupiter.api.Test
 internal class PaginationServiceTest {
     @Test
     fun test() {
-        var calculate = PaginationService(currentPage = 1, pageSize = 5, countItem = 2).calculate()
+        var calculate = PaginationService(currentPage = 1, pageSize = 5, countItem = 2).calculate { "" }
         println(calculate)
 
-        calculate = PaginationService(currentPage = 1, pageSize = 5, countItem = 10).calculate()
+        calculate = PaginationService(currentPage = 1, pageSize = 5, countItem = 10).calculate { "" }
         println(calculate)
 
-        calculate = PaginationService(currentPage = 1, pageSize = 5, countItem = 2).calculate()
+        calculate = PaginationService(currentPage = 1, pageSize = 5, countItem = 2).calculate { "" }
         println(calculate)
 
-        calculate = PaginationService(currentPage = 1, pageSize = 5, countItem = 15).calculate()
+        calculate = PaginationService(currentPage = 1, pageSize = 5, countItem = 15).calculate { "" }
         println(calculate)
 
-        calculate = PaginationService(currentPage = 2, pageSize = 5, countItem = 15).calculate()
+        calculate = PaginationService(currentPage = 2, pageSize = 5, countItem = 15).calculate { "" }
         println(calculate)
     }
 }
